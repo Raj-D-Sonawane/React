09c85405d2750064ba5e2b4f29bf90fe71e043ad
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-30 17:21:25 +0000

    tailwind

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card"
+import State from "./components/State"
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="bg-green-400 text-black p-4 rounded-2xl ">Tailwind test</h1>
+     {/* <Card  name ="raj" />
+     <Card  name  = "ajay" /> */}
+     <State />
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind/src/components/State.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+function State() {
+    const[count,setCount]=useState(1)
+    const addValue = ()=>{
+        setCount(count => count + 1)
+        setCount(count => count + 1)
+        setCount(count => count + 1)
+        setCount(count => count + 1)
+        
+
+        
+    }
+    const removeValue = () => {
+        setCount(count - 1)
+    }
+
+  return (
+    <>
+    <div>{count}</div>
+    <button onClick={addValue}>add</button>
+    <br />
+    <button onClick={removeValue}>remove</button>
+    </>
+    
+  )
+}
+
+export default State
