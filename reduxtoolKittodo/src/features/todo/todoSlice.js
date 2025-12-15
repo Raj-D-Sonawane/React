d29107e6a2f067fefc0b45e7932c44c47c84bbb2
Raj Sonawane <rajsonawane7499@gmail.com>
2025-12-15 06:41:41 +0000

    update todo

--- reduxtoolKittodo/src/features/todo/todoSlice.js
+++ reduxtoolKittodo/src/features/todo/todoSlice.js
@@ -1,4 +1,5 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, TaskAbortError } from "@reduxjs/toolkit";
+import Todos from "../../components/Todos";
 
 const initialState = {
     todos: [{id: 1, text:"Hello world"}]
@@ -18,8 +19,16 @@ export const todoSlice = createSlice({
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
+        updateTodo: (state, action) => {
+            const {id, text} = action.payload;
+            const todo = state.todos.find((todo)=> todo.id === id);
+            if (todo) {
+                Todos.text = text;
+                
+            }
+        }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
-export default todoSlice.reducer
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions;
+export default todoSlice.reducer;
